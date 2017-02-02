9fac498f0784d454c05f5a65d89057e2bdbdf59f
Hari Raghupathy <Hari Raghupathy>
2017-02-01 16:30:50 -0800

    changes to model, code edit on model

--- model.py
+++ model.py
@@ -49,26 +49,27 @@ def normalize_grayscale(image_data):
 	
 X_normalized = normalize_grayscale(X_train)
 
-print('Training...')
-print(len(X_train))
+
 model = Sequential()
-model.add(Convolution2D(24, 5, 5,subsample=(2, 2), input_shape=(66,200,3))) #160, 320, 3)))
-model.add(Activation('relu'))
-model.add(Convolution2D(36, 5, 5,subsample=(2, 2)))
-model.add(Activation('relu'))
-model.add(Convolution2D(48, 5, 5,subsample=(2, 2)))
-model.add(Activation('relu'))
-model.add(Convolution2D(64, 3, 3))
-model.add(Activation('relu'))
-model.add(Convolution2D(64, 3, 3))
-model.add(Activation('relu'))
+
+model.add(Convolution2D(24, 5, 5,subsample=(2, 2), input_shape=(66,200,3),activation='relu')) #160, 320, 3)))
+
+model.add(Convolution2D(36, 5, 5,subsample=(2, 2),activation='relu'))
+
+model.add(Convolution2D(48, 5, 5,subsample=(2, 2),activation='relu'))
+
+model.add(Convolution2D(64, 3, 3,subsample=(1, 1),activation='relu'))
+
+model.add(Convolution2D(64, 3, 3,subsample=(1, 1),activation='relu'))
+
 model.add(Flatten())
-model.add(Dense(100))
-model.add(Activation('relu'))
-model.add(Dense(50))
-model.add(Activation('relu'))
-model.add(Dense(10))
-model.add(Activation('relu'))
+
+model.add(Dense(100,activation='relu'))
+
+model.add(Dense(50,activation='relu'))
+
+model.add(Dense(10,activation='relu'))
+
 model.add(Dense(1))
 
 model.compile('adam', 'mse', ['accuracy'])
