c5d3bf83ff1bfe2088bb2ce234c57cf0b21e081f
Hari Raghupathy <Hari Raghupathy>
2017-02-12 13:10:17 -0800

    code clean up and modular

--- model.py
+++ model.py
@@ -1,49 +1,20 @@
-import csv
+import cv2,sys, getopt
 import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.utils import shuffle
-import cv2,sys
 import matplotlib.pyplot as plt
 from scipy import misc
 from keras.models import Sequential,load_model
 from keras.layers.core import Dense, Activation, Flatten, Dropout, Lambda
 from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
-from myutils import preprocess_input
+from myutils import preprocess_input, read_data_files
 
 X_fname=[]
 y_train = []
-EPOCH = 5
+EPOCH = 2
 DROPOUT = 0.4
-BATCH_SIZE = 128 #28
+BATCH_SIZE = 8 #28
 
-with open('data/driving_log.csv') as csvfile:
-	readCSV = csv.reader(csvfile, delimiter=',')
-	next(readCSV,None)
-	i=0
-	for row in readCSV:
-		file_name = row[0]
-		try:
-			if(file_name.index('Hari')):
-				file_name = file_name.split('\\')[len(file_name.split('\\'))-1]
-				file_name = 'IMG/'+file_name
-		except:
-			file_name = file_name
-		X_fname.append('data/'+file_name)	
-		#img = misc.imread('data/'+file_name,mode='RGB')
-		#X_train.append(preprocess_input(img))
-		y_train.append(row[3].strip())
-		i+=1
-		#if(i==285):
-		#	break
-	#X_train = np.asarray(X_train,dtype=np.float32)
-	X_fname = np.asarray(X_fname)
-	y_train = np.asarray(y_train,dtype=np.float32)
-	
-X_fname, y_train = shuffle(X_fname, y_train)	
-
-X_fname, X_fname_val, y_train, y_validation = train_test_split(X_fname, y_train, test_size=0.10) #, random_state=52)
-
+X_fname, X_fname_val, y_train, y_validation = read_data_files()
 
 def imgGen(files):
 		X_train = []
@@ -59,59 +30,63 @@ def gen_batches(imgs, angles, batch_size):
 	while True:
 		indeces = np.random.choice(num_examples, batch_size)
 		batch_x, batch_y = imgGen(imgs[indeces]), angles[indeces]
-		#if(offset>=num_examples):
-		#	offset = 0
-		#end = offset + batch_size
-		#batch_x, batch_y = imgGen(imgs[offset:end]), angles[offset:end] #imgs[indeces], angles[indeces]
-		#offset = end
 		yield batch_x, batch_y
 		
-#import matplotlib.pyplot as plt
-#plt.imshow(imgGen(['data/IMG/center_2016_12_01_13_35_29_104.jpg']).squeeze())
-#plt.show()		
-		
+def normalize_grayscale(image_data):
+    return image_data/255.0 - 0.5 
+	
 try:
-	if(len(sys.argv)>1):
-		model_file = sys.argv[1]
+	if(len(sys.argv)!=2):
+		print('Usage: python model.py <arg>')
+		print('Where arg can be <filename>.h5 or lenet or nvidia')
+		exit(-1)
+	model_file = sys.argv[1]
 	model = load_model(model_file)
-	print('Model Loaded From Saved h5 file and used')
+	print('Model Loaded From Saved h5 file and used: '+sys.argv[1])
 except:
+	print('New Model built: '+model_file)
 	model = Sequential()
-	
-	model.add(Lambda(lambda x: x/255.0 - 0.5, input_shape=(66,200,3)))
-
-	model.add(Convolution2D(24, 5, 5,subsample=(2, 2),activation='relu')) #160, 320, 3)))
-
-	model.add(Convolution2D(36, 5, 5,subsample=(2, 2),activation='relu'))
-
-	model.add(Convolution2D(48, 5, 5,subsample=(2, 2),activation='relu'))
-	
-	model.add(Dropout(DROPOUT))
-
-	model.add(Convolution2D(64, 3, 3,subsample=(1, 1),activation='relu'))
-
-	model.add(Convolution2D(64, 3, 3,subsample=(1, 1),activation='relu'))
-
-	#model.add(Dropout(DROPOUT))
-
-	model.add(Flatten())
-
-	model.add(Dense(100,activation='relu'))
-	
-	model.add(Dense(50,activation='relu'))
-
-	model.add(Dense(10,activation='relu'))
-
-	model.add(Dense(1))
-
+	if(model_file=='lenet'):
+		model.add(Lambda(lambda x: normalize_grayscale(x), input_shape=(66,200,3)))
+		model.add(Convolution2D(32, 3, 3, border_mode='valid'))
+		model.add(MaxPooling2D(pool_size=(2, 2), border_mode='valid'))
+		model.add(Dropout(DROPOUT))
+		model.add(Activation('relu'))
+		model.add(Convolution2D(32, 3, 3))
+		model.add(MaxPooling2D(pool_size=(2, 2), border_mode='valid'))
+		model.add(Dropout(DROPOUT))
+		model.add(Activation('relu'))
+		model.add(Flatten())
+		model.add(Dense(128))
+		model.add(Activation('relu'))
+		model.add(Dense(43))
+		model.add(Activation('relu'))
+		model.add(Dense(10,activation='relu'))
+		model.add(Dense(1))
+	elif(model_file=='nvidia'):
+		model.add(Lambda(lambda x: normalize_grayscale(x), input_shape=(66,200,3)))
+		model.add(Convolution2D(24, 5, 5,subsample=(2, 2),activation='relu')) #160, 320, 3)))
+		model.add(Convolution2D(36, 5, 5,subsample=(2, 2),activation='relu'))
+		model.add(Convolution2D(48, 5, 5,subsample=(2, 2),activation='relu'))
+		model.add(Dropout(DROPOUT))
+		model.add(Convolution2D(64, 3, 3,subsample=(1, 1),activation='relu'))
+		model.add(Convolution2D(64, 3, 3,subsample=(1, 1),activation='relu'))
+		#model.add(Dropout(DROPOUT))
+		model.add(Flatten())
+		model.add(Dense(100,activation='relu'))
+		model.add(Dense(50,activation='relu'))
+		model.add(Dense(10,activation='relu'))
+		model.add(Dense(1))
+	else:
+		print('Check File Name. Note Usage: python model.py <arg>')
+		print('Where arg can be <filename>.h5 or lenet or nvidia')
+		exit(-1)
 	model.compile('adam', 'mse', ['accuracy'])
+	model_file=model_file+'_model.h5'
 	
-	print('New Model is built')
-
-#history = model.fit(X_train, y_train, nb_epoch=EPOCH, validation_split=0.2)
 history = model.fit_generator(gen_batches(X_fname, y_train, BATCH_SIZE), len(X_fname),EPOCH,validation_data=gen_batches(X_fname_val, y_validation, BATCH_SIZE),nb_val_samples=len(X_fname_val))
+model.save(model_file)  # creates a HDF5 file 'model.h5'
 
-model.save('nvi_model.h5')  # creates a HDF5 file 'model.h5'
-
+#Simple smoke test
 steering_angle = float(model.predict(imgGen(X_fname[1:2]), batch_size=1))
 print('y: %f  predict: %f'%(y_train[1:2],steering_angle))
--- myutils.py
+++ myutils.py
@@ -1,4 +1,8 @@
 import cv2
+import csv 
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.utils import shuffle
 
 def roi(img): 
     img = img[60:140,40:280]
@@ -10,10 +14,33 @@ def preprocess_input(img):
 	
 	
 def normalize_grayscale(image_data):
-    a = -0.5
-    b = 0.5
-    grayscale_min = 0
-    grayscale_max = 255
     return image_data/255.0 - 0.5 
-	#return a + ( ( (image_data - grayscale_min)*(b - a) )/( grayscale_max - grayscale_min ) )
-	
+
+def read_data_files():
+	X_fname=[]
+	y_train = []
+	with open('data/driving_log.csv') as csvfile:
+		readCSV = csv.reader(csvfile, delimiter=',')
+		next(readCSV,None)
+		i=0
+		for row in readCSV:
+			file_name = row[0]
+			try:
+				if(file_name.index('Hari')):
+					file_name = file_name.split('\\')[len(file_name.split('\\'))-1]
+					file_name = 'IMG/'+file_name
+			except:
+				file_name = file_name
+			X_fname.append('data/'+file_name)	
+			y_train.append(row[3].strip())
+			i+=1
+			#if(i==10):
+			#	break
+		X_fname = np.asarray(X_fname)
+		y_train = np.asarray(y_train,dtype=np.float32)
+		
+	X_fname, y_train = shuffle(X_fname, y_train)	
+
+	X_fname, X_fname_val, y_train, y_validation = train_test_split(X_fname, y_train, test_size=0.10) #, random_state=52)
+
+	return X_fname, X_fname_val, y_train, y_validation
